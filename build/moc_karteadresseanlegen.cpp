990f2d2227eba007287777b7cf6929f2dfdb283c
dinhthehuy <dinhthehuy51@gmail.com>
2024-02-25 10:26:56 +0100

    updated build with unit test

--- build/moc_karteadresseanlegen.cpp
+++ build/moc_karteadresseanlegen.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../karteadresseanlegen.h"
+#include "../app/karteadresseanlegen.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
--- build/moc_kartepoianlegen.cpp
+++ build/moc_kartepoianlegen.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../kartepoianlegen.h"
+#include "../app/kartepoianlegen.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
